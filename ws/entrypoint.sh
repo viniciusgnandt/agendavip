e6ee0072ce9a1a44997f7f87ad83007d8af49a02
Vinicius Gnandt <viniciusgnandt@hotmail.com>
2026-01-13 20:43:30 -0300

    c

--- ws/entrypoint.sh
+++ ws/entrypoint.sh
@@ -6,7 +6,11 @@ echo "🔐 Configurando OCI credentials..."
 mkdir -p /root/.oci
 chmod 700 /root/.oci
 
-echo "$OCI_CONFIG" > /root/.oci/config
+# Config
+if [ -n "$OCI_CONFIG" ]; then
+  echo "$OCI_CONFIG" | sed 's/\\n/\n/g' > /root/.oci/config
+fi
+
 echo "$OCI_API_KEY_PEM" > /root/.oci/oci_api_key.pem
 echo "$OCI_API_KEY_PUBLIC_PEM" > /root/.oci/oci_api_key_public.pem
 
