d185533b6a769a0601ddfa0c8ec8e0fdff0e3571
Vinicius Gnandt <viniciusgnandt@hotmail.com>
2026-01-22 20:56:07 -0300

    c

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = process.env.BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const api = axios.create({
   baseURL: BACKEND_URL,
