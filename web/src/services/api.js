f54309c6186006fd283927e5b62b0f37072d7dcd
Vinicius Gnandt <viniciusgnandt@hotmail.com>
2026-01-23 13:30:04 -0300

    c

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const api = axios.create({
-  baseURL: BACKEND_URL,
+  baseURL: 'https://apitest.viniciusgnandt.com.br/' || BACKEND_URL,
 });
 
 export default api;
