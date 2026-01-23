fee9cc320dec80eff542bed6eefba8ea5e94d840
Vinicius Gnandt <viniciusgnandt@hotmail.com>
2026-01-23 16:17:54 -0300

    exporting with consts values

--- web/src/consts.js
+++ web/src/consts.js
@@ -1,9 +1,5 @@
-const salaoId = process.env.REACT_APP_SALAO_ID;
-const clienteId = process.env.REACT_APP_CLIENTE_ID;
-const colaboradorId = process.env.REACT_APP_COLABORADOR_ID;
-
 export default {
-  salaoId,
-  clienteId,
-  colaboradorId,
+  salaoId: '697020abe4aa25c70f59cdfb',
+  clienteId: '6972b3abe4aa25c70f59ce0d',
+  colaboradorId: '6972b331e4aa25c70f59ce05',
 };
--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: 'https://apitest.viniciusgnandt.com.br',
 });
 
 export default api;
