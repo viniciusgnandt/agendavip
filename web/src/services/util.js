ee22859ae127e73abb4f048ce36395770c2d6fc3
Vinicius Gnandt <viniciusgnandt@hotmail.com>
2026-01-25 17:23:53 -0300

    C

--- web/src/services/util.js
+++ web/src/services/util.js
@@ -1,6 +1,6 @@
 export default {
   OCI: {
-    bucketURL: process.env.bucketURL,
+    bucketURL: process.env.BUCKET_URL,
   },
   validateEmail: (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
