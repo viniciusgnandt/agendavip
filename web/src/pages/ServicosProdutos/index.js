b9a374bcda92f63ad29e0f62f04d262ded6ee712
Vinicius Gnandt <viniciusgnandt@hotmail.com>
2026-01-26 14:34:16 -0300

    Fix Imagens do Serviço

--- web/src/pages/ServicosProdutos/index.js
+++ web/src/pages/ServicosProdutos/index.js
@@ -95,7 +95,7 @@ const ServicosProdutos = () => {
     dispatch(removeServico(servicoId));
   };
 
-  useEffect(() => {
+   useEffect(() => {
     dispatch(allServicos());
   }, []);
 
@@ -192,13 +192,12 @@ const ServicosProdutos = () => {
                 multiple
                 autoUpload={false}
                 listType="picture"
-                defaultFileList={servico.arquivos.map((s, i) => ({
-                  name: s?.caminho,
+                defaultFileList={servico.arquivos.map((s, i) => ({                  
+                  name: s?.arquivo,
                   fileKey: i,
-                  url: `${util.OCI.bucketURL}/${s?.caminho}`,
-                  thumbnailUrl: `${util.OCI.bucketURL}/${s?.caminho}`,
-                  status: 'finished',
+                  url: `${util.OCI.bucketURL}/${s?.arquivo}`,
                 }))}
+                               
                 onChange={(files) => {
                   const arquivos = files
                     .filter((f) => f.blobFile)
