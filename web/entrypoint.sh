220a90d4f599760cf38152ae0224de0bf635571b
Vinicius Gnandt <viniciusgnandt@hotmail.com>
2026-01-27 23:01:20 -0300

    c

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "🔧 Gerando env.js em runtime..."
+
 envsubst < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
+
+echo "✅ env.js gerado:"
+cat /usr/share/nginx/html/env.js
 
 exec "$@"
